ef070bc712609da9a4f6b072851f1f904981410b
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-29 15:23:45 -0600

    Agregando logo al Header

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,23 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
     <header className={styles.header}>
         <div className="contenedor">
-            <div>
+            <div className={styles.barra}>
+                <Link href="/">
+                    <Image src="/img/logo.svg" alt="GuitarLA" width={400} height={100} />
+                </Link>
 
+                <nav className={styles.navegacion}>
+                    <Link href="/">Inicio</Link>
+                    <Link href="/nosotros">Nosotros</Link>
+                    <Link href="/blog">Blog</Link>
+                    <Link href="/tienda">Tienda</Link>
+                </nav>
             </div>
-
-            <nav className={styles.navegacion}>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Nosotros</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/tienda">Tienda</Link>
-            </nav>
         </div>
 
     </header>
