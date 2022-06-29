e6b5a49983a89d9d65b0e2dc384d47de5823a539
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-29 14:39:34 -0600

    Agregando layout

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import Link from 'next/link'
+import Layout from '../components/Layout'
 
-const nosotros = () => {
+const Nosotros = () => {
   return (
-    <div>
-      
-    </div>
+    <Layout
+        pagina='Nosotros'
+    >
+        <h1>Desde Nosotros</h1>
+    </Layout>
   )
 }
 
-export default nosotros
+export default Nosotros
