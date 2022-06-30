5e982248081958db2c7d2af26f0ce54275ce772e
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-29 19:36:23 -0600

    Se agregó Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Header from "./Header"
+import Footer from "./Footer"
 
 const Layout = ({children, pagina}) => {
   return (
@@ -12,6 +13,8 @@ const Layout = ({children, pagina}) => {
         <Header />
 
         {children}
+
+        <Footer />
     </div>
   )
 }
