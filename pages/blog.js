25f4541673ff80ae50af45ca4e88276a82fcbefb
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-29 19:32:52 -0600

    Sección Blog agregada

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+const Blog = () => {
+  return (
+    <Layout
+        pagina='Blog'
+    >
+        <h1>Desde Blog</h1>
+    </Layout>
+  )
+}
+
+export default Blog
