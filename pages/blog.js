a311748f0828d08febbcd9381fe01a282403da5a
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-01 16:54:14 -0600

    Modificando page blog para mostrar entradas

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,42 @@
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import Entrada from "../components/Entrada";
 
-const Blog = () => {
+
+
+const Blog = ({entradas}) => {
+
   return (
-    <Layout
-        pagina='Blog'
+    <Layout 
+      pagina="Blog"
     >
-        <h1>Desde Blog</h1>
+
+      <main className='contenedor'>
+          <h2 className='heading'>Blog</h2>
+
+          <div>
+              {entradas.map(entrada => (
+                <Entrada
+                  key={entrada.id}
+                  entrada={entrada}
+                />
+              ))}
+          </div>
+      </main>
     </Layout>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const url = "http://localhost:1337/blogs";
+  const respuesta = await fetch(url);
+  const entradas = await respuesta.json();
+  console.log(entradas);
+
+  return {
+    props: {
+      entradas
+    }
+  }
 }
 
-export default Blog
+export default Blog;
