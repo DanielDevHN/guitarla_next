bcf0547fc602a01fda0ef078f675e7561ec5a8ce
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-02 02:16:23 -0600

    Se agregó pagina para extraer entradas de la API

---
+++ pages/blog/[id].js
@@ -0,0 +1,73 @@
+import Image from "next/image"
+import Layout from "../../components/Layout"
+import { formatearFecha } from "../../helpers"
+import styles from '../../styles/Entrada.module.css'
+
+const EntradaBlog = ({entrada}) => {
+
+    const {contenido, imagen, published_at, titulo} = entrada
+
+    return (
+        <Layout>
+
+        
+            <main className="contenedor">
+                <h1 className="heading">{titulo}</h1>
+                <article className={styles.entrada}>
+                    <Image layout='responsive' width={800} height={600}
+                    src={imagen.url} alt={`Imagen entrada ${titulo}`} />
+
+                    <div className={styles.contenido}>
+                        <p className={styles.fecha}>{formatearFecha(published_at)}</p>
+                        <p className={styles.texto}>{contenido}</p>
+                    </div>
+                </article>
+            </main>
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const url = `${process.env.API_URL}/blogs`
+    const respuesta = await fetch(url)
+    const entradas = await respuesta.json()
+
+    const paths = entradas.map(entrada => ({
+        params: { id: entrada.id }
+    }))
+
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params: {id}}) {
+    const url = `${process.env.API_URL}/blogs/${id}`
+    const respuesta = await fetch(url)
+    const entrada = await respuesta.json()
+
+    return  {
+        props: {
+            entrada
+        }
+
+    }
+}
+
+
+/*export async function getServerSideProps({params: {id}}) {
+    const url = `${process.env.API_URL}/blogs/${id}`;
+    const respuesta = await fetch(url)
+    const entrada = await respuesta.json()
+
+    return  {
+        props: {
+            entrada
+        }
+
+    }
+}*/
+
+export default EntradaBlog
