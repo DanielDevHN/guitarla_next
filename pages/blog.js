e4498b8267f018e9c9186ed0e312fd5926e4f17d
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-01 19:47:17 -0600

    Se agregaron estilos CSS

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import Entrada from "../components/Entrada";
-
+import styles from '../styles/Blog.module.css';
 
 
 const Blog = ({entradas}) => {
@@ -13,7 +13,7 @@ const Blog = ({entradas}) => {
       <main className='contenedor'>
           <h2 className='heading'>Blog</h2>
 
-          <div>
+          <div className={styles.blog}>
               {entradas.map(entrada => (
                 <Entrada
                   key={entrada.id}
