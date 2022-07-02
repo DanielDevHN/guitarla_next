8fb73597334a8c6abeb0e90ed6a98d330324270a
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-01 19:46:51 -0600

    Se corrigieron errores en los links

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,10 @@ const Header = () => {
         <div className="contenedor">
             <div className={styles.barra}>
                 <Link href="/">
-                    <Image src="/img/logo.svg" alt="GuitarLA" width={400} height={100} />
+                    <a>
+                        <Image src="/img/logo.svg" alt="GuitarLA" 
+                        width={400} height={100} />
+                    </a>
                 </Link>
 
                 <nav className={styles.navegacion}>
