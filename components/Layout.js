9007c550fca86d68dd51ad830a7140331f1a726a
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-03 01:45:17 -0600

    Se agregó nueva fuente para los textos

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,9 @@ const Layout = ({children, pagina}) => {
         <Head>
             <title>GuitarLA - {pagina}</title>
             <meta name="description" content="Sitio Web de venta de guitarras" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap" rel="stylesheet" />
         </Head>
         
         <Header />
