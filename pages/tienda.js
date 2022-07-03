8d01e850cd620c0cd2a8f5f74b3b2bb6b02c5c52
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-03 01:46:54 -0600

    Se agregó filtro para resultados de API

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,13 +1,35 @@
 import Layout from '../components/Layout'
+import Listado from '../components/Listado'
 
-const Tienda = () => {
+const Tienda = ({guitarras}) => {
+
   return (
     <Layout
         pagina='Tienda Virtual'
     >
-        <h1>Desde Tienda</h1>
+       <main className='contenedor'>
+          <h1 className='heading'>Nuestra Colección</h1>
+
+          <Listado 
+            guitarras={guitarras}
+          />
+       </main>
     </Layout>
   )
+}
+
+
+export async function getServerSideProps() {
+  
+  const url = `${process.env.API_URL}/guitarras?_sort=created_at:desc`
+  const respuesta = await fetch(url)
+  const guitarras = await respuesta.json()
+
+    return {
+        props: {
+            guitarras
+        }
+    }
 }
 
 export default Tienda
