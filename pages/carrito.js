040e4c02957f42aa91f91ad6c386a6626bba6711
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-05 17:51:55 -0600

    Se agregó page para carrito de compras

---
+++ pages/carrito.js
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout"
+import styles from '../styles/Carrito.module.css'
+
+const Carrito = ({carrito}) => {
+
+  return (
+    <Layout
+        pagina={'Carrito de Compras'}
+    >
+        <h1 className="heading">Carrito</h1>
+        <main className={`contenedor ${styles.contenido}`}>
+            <div>1</div>
+            <div>2</div>
+        </main>
+
+    </Layout>
+  )
+}
+
+export default Carrito
