de8da079313190744eb89134958d4e3c1a882af6
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-04 21:01:27 -0600

    Se agregó page para mostrar producto seleccionado

---
+++ pages/guitarras/[url].js
@@ -0,0 +1,65 @@
+import Image from "next/image"
+import Layout from "../../components/Layout"
+import styles from '../../styles/Guitarra.module.css'
+
+
+const Producto = ({guitarra}) => {
+  
+    const { descripcion, imagen, nombre, precio } = guitarra[0]
+  
+    return (
+        <Layout
+            pagina={`Guitarra ${nombre}`}
+        >
+            <div className={styles.guitarra}>
+                <Image  primary="true" layout='responsive' width={150} height={350}
+                src={imagen.url} alt={`Imagen Guitarra ${nombre}`} />
+                <div className={styles.contenido}>
+                    <h3>{nombre}</h3>
+                    <p className={styles.descripcion}>{descripcion}</p>
+                    <p className={styles.precio}>${precio}</p>
+
+                    <form className={styles.formulario}>
+                        <label className={styles.label}>Cantidad:</label>
+
+                        <select className={styles.select}>
+                            <option value=""> -- Seleccione --</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+
+                        <input
+                            type="submit"
+                            value="Agregar al carrito"
+                        />
+                    </form>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+
+export async function getServerSideProps({query: {url}}) {
+    const urlGuitarra = `${process.env.API_URL}/guitarras?url=${url}`
+    const respuesta = await fetch(urlGuitarra)
+    const guitarra = await respuesta.json()
+
+    return {
+        props: {
+           guitarra
+        }
+    }
+}
+
+
+
+export default Producto
