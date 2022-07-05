e3c375d289980227cfed35dedd5a51629f6aa2b3
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-05 17:58:29 -0600

    Se agregó imagen para carrito de compras en Header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import styles from '../styles/Header.module.css'
 
-const Header = () => {
+const Header = ({guitarra}) => {
+
+  const router = useRouter()
+
   return (
     <header className={styles.header}>
         <div className="contenedor">
@@ -19,9 +23,34 @@ const Header = () => {
                     <Link href="/nosotros">Nosotros</Link>
                     <Link href="/blog">Blog</Link>
                     <Link href="/tienda">Tienda</Link>
+                    <Link href="/carrito">
+                        <a>
+                            <Image layout="fixed" width={30} height={25} 
+                            src="/img/carrito.png" alt="Imagen Carrito" />
+                        </a>
+                    </Link>
                 </nav>
             </div>
+
+            {guitarra && (
+                <div className={styles.modelo}>
+                    <h2>Modelo {guitarra.nombre}</h2>
+                    <p>{guitarra.descripcion}</p>
+                    <p className={styles.precio}>${guitarra.precio}</p>
+                    <Link href={`/guitarras/${guitarra.url}`}>
+                        <a className={styles.enlace}>Ver Producto</a>
+                    </Link>
+                </div>
+            )}
         </div>
+
+        {router.pathname === '/' && (
+            <div className={styles.guitarra}>
+                 <Image layout='fixed' width={350} height={800}  
+                 src='/img/header_guitarra.png' alt="imagen header guitarra" />
+            </div>
+           
+        )}
 
     </header>
   )
