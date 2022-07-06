5d67e6614ad272ecd40030d3a87523fb58a6b80d
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-05 18:00:32 -0600

    Se agregó un defaultprops para mostrar imagen en Header

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 import Header from "./Header"
 import Footer from "./Footer"
 
-const Layout = ({children, pagina}) => {
+const Layout = ({children, pagina, guitarra}) => {
   return (
     <div>
         <Head>
@@ -13,13 +13,19 @@ const Layout = ({children, pagina}) => {
             <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap" rel="stylesheet" />
         </Head>
         
-        <Header />
+        <Header 
+          guitarra={guitarra}
+        />
 
         {children}
 
         <Footer />
     </div>
   )
+}
+
+Layout.defaultProps = {
+  guitarra: null
 }
 
 export default Layout
