1df0cada65ff259e64d67b8b84e375dedac42d79
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-05 18:25:29 -0600

    Se agregaron props para obtener listado

--- components/Listado.js
+++ components/Listado.js
@@ -1,7 +1,7 @@
 import Guitarra from "./Guitarra"
 import styles from "../styles/Listado.module.css"
 
-const Listado = ({guitarras}) => {
+const Listado = ({ guitarras }) => {
   return (
     <div className={styles.listado}>
        {guitarras.map(guitarra => (
