17eca6eb17989568e1d6a140a944adc81480901b
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-05 18:04:23 -0600

    Se agregaron componentes y funciones de consulta de API

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,62 @@
 import Layout from '../components/Layout'
+import Listado from '../components/Listado'
+import Curso from '../components/Curso'
+import ListadoBlog from '../components/ListadoBlog'
 
+export default function Home({guitarras, cursos, entradas}) {
 
-export default function Home() {
   return (
       <Layout
         pagina='Inicio'
+        guitarra = {guitarras[8]}
       >
-          <h1>Desde Inicio</h1>
+          <main className='contenedor'>
+            <h1 className='heading'>Nuestra Colección</h1>
+
+            <Listado 
+              guitarras={guitarras}
+            />
+          </main>
+
+          <Curso 
+            cursos={cursos}
+          />
+
+          <section className='contenedor'>
+            <ListadoBlog 
+              entradas={entradas}
+            />
+          </section>
+
       </Layout>
   )
 }
+
+
+export async function getServerSideProps() {
+  
+  const urlGuitarras = `${process.env.API_URL}/guitarras`
+  const urlCursos = `${process.env.API_URL}/cursos`
+  const urlBlog = `${process.env.API_URL}/blogs?_limit=3&_sort=created_at:desc`;
+
+  const [respGuitarras, respCursos, respEntradas] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlCursos),
+    fetch(urlBlog)
+  ])
+
+  const [guitarras, cursos, entradas] = await Promise.all([
+    respGuitarras.json(),
+    respCursos.json(),
+    respEntradas.json()
+  ])
+
+  return {
+      props: {
+        guitarras,
+        cursos,
+        entradas
+    }
+  }
+}
+
