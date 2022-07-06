cf794ebc7de251fc32f376ee6474faa9f525022e
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-07-05 18:02:52 -0600

    Se agregó un segmento para iterar blogs

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,5 @@
 import Layout from "../components/Layout";
-import Entrada from "../components/Entrada";
-import styles from '../styles/Blog.module.css';
+import ListadoBlog from "../components/ListadoBlog";
 
 
 const Blog = ({entradas}) => {
@@ -11,16 +10,9 @@ const Blog = ({entradas}) => {
     >
 
       <main className='contenedor'>
-          <h2 className='heading'>Blog</h2>
-
-          <div className={styles.blog}>
-              {entradas.map(entrada => (
-                <Entrada
-                  key={entrada.id}
-                  entrada={entrada}
-                />
-              ))}
-          </div>
+         <ListadoBlog 
+            entradas={entradas}
+         />
       </main>
     </Layout>
   );
